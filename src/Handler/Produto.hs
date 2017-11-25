8939df6556f47ec7830ae6578ef08fe59dfc9377
Haskell <algarcia@usp.br>
2017-11-25 21:31:40 +0000

    ADRIANA REIS (8) - Criação da rota e Metodo Get na tabela Produto

--- src/Handler/Produto.hs
+++ src/Handler/Produto.hs
@@ -14,3 +14,8 @@ postProdutoInsereR = do
     produto <- requireJsonBody :: Handler Produto
     pid <- runDB $ insert produto
     sendStatusJSON created201 (object ["data" .= (fromSqlKey pid)]
+    
+getProdutoWithIdR :: ProdutoId -> Handler Value
+getProdutoWithIdR pid = do 
+    produto <- runDB $ get404 pid
+    sendStatusJSON ok200 (object ["data" .= (toJSON produto)])
