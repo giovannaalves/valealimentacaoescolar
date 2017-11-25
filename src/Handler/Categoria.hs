8056d244d9529b1689e3f004ad9f13b2ccaa1851
Haskell <algarcia@usp.br>
2017-11-25 21:23:22 +0000

    ADRIANA REIS (6) - Alteração das rotas e Metodo Put na tabela Categoria

--- src/Handler/Categoria.hs
+++ src/Handler/Categoria.hs
@@ -15,13 +15,20 @@ postCategoriaInsereR = do
     cid <- runDB $ insert categoria
     sendStatusJSON created201 (object ["data" .= (fromSqlKey cid)])
     
-getCategoriaBuscarR :: CategoriaId -> Handler Value
-getCategoriaBuscarR cid = do 
+getCategoriaWithIdR :: CategoriaId -> Handler Value
+getCategoriaWithIdR cid = do 
     categoria <- runDB $ get404 cid
     sendStatusJSON ok200 (object ["data" .= (toJSON categoria)])
 
-deleteCategoriaApagarR :: CategoriaId -> Handler Value
-deleteCategoriaApagarR cid = do 
+deleteCategoriaWithIdR :: CategoriaId -> Handler Value
+deleteCategoriaWithIdR cid = do 
     _ <- runDB $ get404 cid
     runDB $ delete cid
     sendStatusJSON noContent204 (object ["data" .= (fromSqlKey cid)])
+
+putCategoriaWithIdR :: CategoriaId -> Handler Value
+putCategoriaWithIdR cid = do
+    _ <- runDB $ get404 cid
+    novaCategoria <- requireJsonBody :: Handler Categoria
+    runDB $ replace aid novaCategoria
+    sendStatusJSON noContent204 (object ["data" .= (fromSqlKey cid)]) 
