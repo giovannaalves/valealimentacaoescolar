d9265b39cd6b7379100e1161ef03f8f3ce2427e5
Haskell <algarcia@usp.br>
2017-11-29 20:51:08 +0000

    GIOVANNA ALVES (12) - Adição do método GetRestriçõesByAluno e rota

--- src/Handler/Restricao.hs
+++ src/Handler/Restricao.hs
@@ -14,3 +14,10 @@ postRestricaoInsereR = do
     rid <- runDB $ insert restricao
     sendStatusJSON created201 (object ["data" .= (fromSqlKey rid)])
 
+getRestricaoByAlunoR :: AlunoId -> Handler Value
+getRestricaoByAlunoR rid = do 
+    restricoes' <- runDB $ selectList [RestricaoIdAluno ==. rid] []
+    restricoes <- return $ fmap (\(Entity _ alo) -> alo) restricoes'
+    pids <- return $ fmap restricaoIdProduto restricoes 
+    produtos <- sequence $ fmap (\pid -> runDB $ get404 pid) pids
+    sendStatusJSON ok200 (object ["data" .= (toJSON produtos)])
