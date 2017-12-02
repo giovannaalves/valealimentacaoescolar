b9e3c8e628472f32ea7d9f8640ccd834723bb610
Haskell <algarcia@usp.br>
2017-12-02 23:57:34 +0000

    INGRID BIANCA (23) - Ajuestes em Alergen- Produto (enable cors e rota)

--- src/Handler/AlergenoProduto.hs
+++ src/Handler/AlergenoProduto.hs
@@ -6,24 +6,34 @@
 module Handler.AlergenoProduto where
 
 import Import
-import Network.HTTP.Types.Status
+--import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import Handler.EnableCors
 
-postAlergenoProdutoR :: ProdutoId -> AlergenoId -> Handler Value
-postAlergenoProdutoR produtoid alergenoid = do
-    alergenoproduto <- return $ AlergenoProduto produtoid alergenoid
-    ppid <- runDB $ insert alergenoproduto
-    sendStatusJSON created201 (object ["data" .= (fromSqlKey ppid)])
+optionsAlergenoByProdutoR :: ProdutoId -> Handler Value
+optionsAlergenoByProdutoR _ = optionGenerico "OPTIONS, GET"
 
 getAlergenoByProdutoR :: ProdutoId -> Handler Value
 getAlergenoByProdutoR ppid = do 
+    addCorsHeader "GET"
     alergenos' <- runDB $ selectList [AlergenoProdutoProdid ==. ppid] []
     alergenos <- return $ fmap (\(Entity _ alo) -> alo) alergenos'
     pids <- return $ fmap alergenoProdutoAlergid alergenos 
     alergenos <- sequence $ fmap (\pid -> runDB $ get404 pid) pids
     sendStatusJSON ok200 (object ["data" .= (toJSON alergenos)])
+    
+optionsAlergenoProdutoR :: ProdutoId -> AlergenoId -> Handler Value
+optionsAlergenoProdutoR _ _ = optionGenerico "OPTIONS, POST, DELETE"
+    
+postAlergenoProdutoR :: ProdutoId -> AlergenoId -> Handler Value
+postAlergenoProdutoR produtoid alergenoid = do
+    addCorsHeader "POST"
+    alergenoproduto <- return $ AlergenoProduto produtoid alergenoid
+    ppid <- runDB $ insert alergenoproduto
+    sendStatusJSON created201 (object ["data" .= (fromSqlKey ppid)])
 
 deleteAlergenoProdutoR :: ProdutoId -> AlergenoId -> Handler Value
-deleteAlergenoProdutoR pid aid = do 
+deleteAlergenoProdutoR pid aid = do
+    addCorsHeader "DELETE"
     runDB $ deleteBy $ UniqueAlergenoProduto pid aid
     sendStatusJSON noContent204 emptyObject
