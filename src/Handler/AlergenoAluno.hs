7aa3027c97a49c5b13c15a7c881ed1b733735c96
Haskell <algarcia@usp.br>
2017-12-02 15:57:41 +0000

    GIOVANNA ALVES (18) - Adição do método GetAlergenosByAluno e ajustes nas rotas

--- src/Handler/AlergenoAluno.hs
+++ src/Handler/AlergenoAluno.hs
@@ -13,3 +13,11 @@ postAlergenoAlunoInsereR = do
     alergenoaluno <- requireJsonBody :: Handler AlergenoAluno
     aaid <- runDB $ insert alergenoaluno
     sendStatusJSON created201 (object ["data" .= (fromSqlKey aaid)])
+
+getAlergenoByAlunoR :: AlunoId -> Handler Value
+getAlergenoByAlunoR aaid = do 
+    alergenos' <- runDB $ selectList [AlergenoAlunoIdAluno ==. aaid] []
+    alergenos <- return $ fmap (\(Entity _ alo) -> alo) alergenos'
+    aids <- return $ fmap alergenoAlunoIdAlergeno alergenos 
+    alergenos <- sequence $ fmap (\aid -> runDB $ get404 aid) aids
+    sendStatusJSON ok200 (object ["data" .= (toJSON alergenos)])
