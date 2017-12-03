3419f0b3f05db717c12aeb84dde2325585b23f1c
Haskell <algarcia@usp.br>
2017-12-03 15:19:40 +0000

    ADRIANA REIS (26) - Ajustes em ItemCompra (Options e Enable cors)

--- src/Handler/ItemCompra.hs
+++ src/Handler/ItemCompra.hs
@@ -8,20 +8,30 @@ module Handler.ItemCompra where
 import Import
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import Handler.EnableCors
+
+optionsItemCompraInsereR :: Handler Value
+optionsItemCompraInsereR = optionGenerico "OPTIONS, GET, POST"
 
 postItemCompraInsereR :: Handler Value
 postItemCompraInsereR = do
+    addCorsHeader "POST"
     itemCompra <- requireJsonBody :: Handler Compra
     iid <- runDB $ insert itemCompra
     sendStatusJSON created201 (object ["data" .= (fromSqlKey iid)])
-    
+
+optionsItemCompraWithIdR :: ItemCompraId -> Handler Value
+optionsItemCompraWithIdR _ = optionGenerico "OPTIONS, GET, PUT, DELETE"
+
 getItemCompraWithIdR :: ItemCompraId -> Handler Value
-getItemCompraWithIdR iid = do 
+getItemCompraWithIdR iid = do
+    addCorsHeader "GET"
     itemCompra <- runDB $ get404 iid
     sendStatusJSON ok200 (object ["data" .= (toJSON itemCompra)])
     
 putItemCompraWithIdR :: ItemCompraId -> Handler Value
 putItemCompraWithIdR iid = do
+    addCorsHeader "PUT"
     _ <- runDB $ get404 iid
     novoItemCompra <- requireJsonBody :: Handler ItemCompra
     runDB $ replace iid novoItemCompra
@@ -29,6 +39,7 @@ putItemCompraWithIdR iid = do
     
 deleteItemCompraWithIdR :: ItemCompraId -> Handler Value
 deleteItemCompraWithIdR iid = do 
+    addCorsHeader "DELETE"
     _ <- runDB $ get404 iid
     runDB $ delete iid
     sendStatusJSON noContent204 (object ["data" .= (fromSqlKey iid)])
