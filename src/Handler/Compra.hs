b70b0ab5abc04bbd7f5979cd46d31a605e3755d3
Haskell <algarcia@usp.br>
2017-12-03 15:15:16 +0000

    ADRIANA REIS (25) - Ajustes em Compra (Options e Enable cors)

--- src/Handler/Compra.hs
+++ src/Handler/Compra.hs
@@ -12,6 +12,7 @@ import Import
 import Database.Persist.Postgresql
 import Data.Time.Clock
 import Data.Time.Calendar
+import Handler.EnableCors
 
 data CompraDto = CompraDto
     { idAluno :: AlunoId,
@@ -23,21 +24,30 @@ data CompraDto = CompraDto
 instance FromJSON CompraDto
 instance ToJSON CompraDto 
 
+optionsCompraInsereR :: Handler Value
+optionsCompraInsereR = optionGenerico "OPTIONS, GET, POST"
+
 postCompraInsereR :: Handler Value
 postCompraInsereR = do
+    addCorsHeader "POST"
     compra <- requireJsonBody :: Handler Compra
     cid <- runDB $ insert compra
     contacorrente <- return $ ContaCorrente (compraIdAluno compra) "2017-12-02" 1 (compraTotal compra)
     _ <- runDB $ insert contacorrente
     sendStatusJSON created201 (object ["data" .= (fromSqlKey cid)])
+    
+optionsCompraWithIdR :: CompraId -> Handler Value
+optionsCompraWithIdR _ = optionGenerico "OPTIONS, GET, PUT, DELETE"
 
 getCompraWithIdR :: CompraId -> Handler Value
-getCompraWithIdR cid = do 
+getCompraWithIdR cid = do
+    addCorsHeader "GET"
     compra <- runDB $ get404 cid
     sendStatusJSON ok200 (object ["data" .= (toJSON compra)])
     
 putCompraWithIdR :: CompraId -> Handler Value
 putCompraWithIdR cid = do
+    addCorsHeader "PUT"
     _ <- runDB $ get404 cid
     novaCompra <- requireJsonBody :: Handler Compra
     runDB $ replace cid novaCompra
@@ -45,12 +55,14 @@ putCompraWithIdR cid = do
 
 deleteCompraWithIdR :: CompraId -> Handler Value
 deleteCompraWithIdR cid = do 
+    addCorsHeader "DELETE"
     _ <- runDB $ get404 cid
     runDB $ delete cid
     sendStatusJSON noContent204 (object ["data" .= (fromSqlKey cid)])
     
 postComprarR :: AlunoId -> Handler Value
 postComprarR aid = do
+    addCorsHeader "POST"
     compraDto <- requireJsonBody :: Handler CompraDto
     let itensDaCompra = itens compraDto
     let total = sum $ map (\ic -> (itemCompraValorItem ic) * (fromIntegral (itemCompraQuantidade ic))) itensDaCompra
